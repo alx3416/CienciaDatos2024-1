b03f9639cfe058477a0c44d95b573e17253ed350
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-02-12 18:22:25 -0600

    adding checkfolder function
    gitignore updated

--- utils/processing.py
+++ utils/processing.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 
 def check_output_folder(path):
-    # Revisar si existe carpeta output
-    # Si no existe, crearla
-    raise NotImplementedError
-
+    if os.path.isdir(path):
+        pass
+    else:
+        os.makedirs(path)
+        print(path + " folder created")
