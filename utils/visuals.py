b4cf39ba2d0327bbb10ec065bb04643be23cceb0
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-02-19 19:40:13 -0600

    autoformato PEP8

--- utils/visuals.py
+++ utils/visuals.py
@@ -12,7 +12,7 @@ def save_histogram(data, column):
     proc.check_output_folder("output/histograms")
     sns_plot = sns.histplot(data=data[column])
     fig = sns_plot.get_figure()
-    fig.savefig("output/histograms/histogram_"+column+".png")
+    fig.savefig("output/histograms/histogram_" + column + ".png")
     plt.close()
 
 
@@ -24,33 +24,34 @@ def save_histograms(data):
 
 
 def save_correlation(data, var1, var2):
-        # Verificar si la carpeta "output" existe, si no, crearla
-        proc.check_output_folder("output/scatterplots")
+    # Verificar si la carpeta "output" existe, si no, crearla
+    proc.check_output_folder("output/scatterplots")
 
-        # Calcular la correlación entre las dos variables
-        correlation_value = data[var1].corr(data[var2])
+    # Calcular la correlación entre las dos variables
+    correlation_value = data[var1].corr(data[var2])
 
-        # Crear el gráfico de correlación
-        sns.scatterplot(data=data, x=var1, y=var2)
+    # Crear el gráfico de correlación
+    sns.scatterplot(data=data, x=var1, y=var2)
 
-        # Añadir el valor de correlación como parte del título
-        plt.title(f"Correlación entre {var1} y {var2}: {correlation_value}")
-        plt.xlabel(var1)
-        plt.ylabel(var2)
+    # Añadir el valor de correlación como parte del título
+    plt.title(f"Correlación entre {var1} y {var2}: {correlation_value}")
+    plt.xlabel(var1)
+    plt.ylabel(var2)
 
-        # Guardar el gráfico como imagen PNG
-        plt.savefig(f"output/scatterplots/{var1}_{var2}_correlation_plot.png")
+    # Guardar el gráfico como imagen PNG
+    plt.savefig(f"output/scatterplots/{var1}_{var2}_correlation_plot.png")
 
-        # Limpiar la figura
-        plt.clf()
-        plt.close()
-        pass
+    # Limpiar la figura
+    plt.clf()
+    plt.close()
+    pass
 
 
 def save_correlations(data, correlations):
     # create each scatter plot for var1 and var2
     # save each plot
     pass
+
 
 def save_all_correlations(data, correlations):
     pass
