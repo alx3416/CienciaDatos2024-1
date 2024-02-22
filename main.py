525e923717587816129cf9e387e9c7e6c193909c
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-02-21 20:01:52 -0600

    fixing conflict and autoformat

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ import utils.visuals as visu
 # cargar dataset diabetes desde folder data
 dataset = imp.read_diabetes_dataset("data/diabetes.tab.txt")
 
-
 # Generar y guardar los histogramas para una exploración inicial
 visu.save_histogram(dataset, "AGE")
 visu.save_histograms(dataset)
-<<<<<<< Updated upstream
-=======
-visu.save_correlation(dataset, "BMI","S6")
+visu.save_correlation(dataset, "BMI", "S6")
 visu.save_all_correlations(dataset, dataset.corr())
->>>>>>> Stashed changes
