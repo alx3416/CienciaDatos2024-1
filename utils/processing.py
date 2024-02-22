cda824983b4965279a44570aef556899cfd2ab12
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-02-21 20:33:42 -0600

    adding split data and linear model functions

--- utils/processing.py
+++ utils/processing.py
@@ -1,6 +1,7 @@
 import os
 import math as mt
 import pandas as pd
+from sklearn import linear_model
 
 
 def check_output_folder(path):
@@ -40,3 +41,19 @@ def normalize_diabetes_data(data):
     normalized_data = normalized_data.mul(val, axis='columns')
     normalized_data["Y"] = data["Y"]
     return normalized_data
+
+
+def split_data(data, split_percentage):
+    training_data = data.sample(frac=split_percentage)
+    idx_orig = data.index
+    idx_train = training_data.index
+    idx_test = idx_orig.drop(idx_train)
+    test_data = data.loc[idx_test]
+    return training_data, test_data
+
+
+def simple_linear_regression(input_train, output_train):
+    regr = linear_model.LinearRegression()
+    inputs = input_train.values.reshape(-1, 3)
+    regr.fit(inputs, output_train)
+    return regr
