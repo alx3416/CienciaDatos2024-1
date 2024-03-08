becb2292c4654a199de11323f1fd8eea93fa56e9
Forge <angelxone@outlook.com>
2024-03-08 08:04:55 -0600

    read function
    agregue función de read

--- utils/imports.py
+++ utils/imports.py
@@ -5,3 +5,7 @@ def read_diabetes_dataset(path):
     # Utilizar pandas para cargar el txt como objeto dataframe
     data = pd.read_csv(path, sep="\t")
     return data
+
+def read_inegi_datset(path):
+    data2 =pd.read_csv(path, sep="\t")
+    return data2
