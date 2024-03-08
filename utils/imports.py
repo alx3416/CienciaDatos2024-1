203f14900fe3caed285ca930d3f99b09e06c535a
21Hell <76609684+21Hell@users.noreply.github.com>
2024-03-08 13:22:32 -0600

    tomates

--- utils/imports.py
+++ utils/imports.py
@@ -5,3 +5,21 @@ def read_diabetes_dataset(path):
     # Utilizar pandas para cargar el txt como objeto dataframe
     data = pd.read_csv(path, sep="\t")
     return data
+
+#Read the file .xlsx
+def read_excel(path):
+    # Utilizar pandas para cargar el txt como objeto dataframe
+    data = pd.read_excel(path)
+    return data
+
+#printeamos todos los archivos de un directorio
+def print_files_in_directory(path):
+    import os
+    for dirname, _, filenames in os.walk(path):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
+
+def read_csv_tomates(path):
+    # Utilizar pandas para cargar el txt como objeto dataframe
+    data = pd.read_csv(path)
+    return data
