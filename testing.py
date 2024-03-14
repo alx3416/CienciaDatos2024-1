71fe13e0c6ca420f3d31ed52ad1898cc813a8108
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-03-14 15:43:23 -0600

    minor fix in testing

--- testing.py
+++ testing.py
@@ -1,5 +1,9 @@
-import main as mn
+import utils.processing as proc
 
 
 def test_suma_valores_1(a=2, b=2):
     assert proc.suma_valores(a, b) == 4
+
+
+def test_suma_valores_2(a=-2, b=7):
+    assert proc.suma_valores(a, b) == 5
